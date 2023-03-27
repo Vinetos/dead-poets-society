bf4992dccf9e19697c03e92f41fdbe75793fd575
Valentin Chassignol <Vinetosdev@gmail.com>
2023-03-27 13:06:22 +0200

    wip: Models 2

--- src/main/java/com/voltaired/voltaired/data/model/LetterModel.java
+++ src/main/java/com/voltaired/voltaired/data/model/LetterModel.java
@@ -5,11 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 import lombok.With;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
