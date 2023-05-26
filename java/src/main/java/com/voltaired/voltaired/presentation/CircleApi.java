c1c9ded5715a92d84f2b984af975cf1c4eb12fd3
Valentin Chassignol <Vinetosdev@gmail.com>
2023-05-26 14:26:04 +0200

    wip: RBAC policy

--- java/src/main/java/com/voltaired/voltaired/presentation/CircleApi.java
+++ java/src/main/java/com/voltaired/voltaired/presentation/CircleApi.java
@@ -5,7 +5,14 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.With;
 
-import javax.ws.rs.*;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import java.time.ZonedDateTime;
 import java.util.List;
 
@@ -13,7 +20,7 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 @Path("/circles") @Produces(APPLICATION_JSON) @Consumes(APPLICATION_JSON) public interface CircleApi {
 
-    @GET List<getAllCircles.Response> getAllActivities();
+    @GET List<getAllCircles.Response> getAllCircles();
 
     @GET @Path("{id}") getCircle.Response getCircle(@PathParam("id") Long id);
 
@@ -28,6 +35,16 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
     @POST
     @Authenticated
     @Path("{id}/letters") postLetters.Response postLetters(@PathParam("id") Long id, postLetters.Request request);
+
+    @POST
+    @Authenticated
+    @Path("{id}/letters/{letterId}") postLettersReply.Response postLetterReply(@PathParam("id") Long id,
+                                                                          @PathParam("letterId") Long letterId,
+                                                                               postLettersReply.Request request);
+
+    @DELETE
+    @Authenticated
+    @Path("{id}") deleteCircles deleteCircles(@PathParam("id") Long id);
 
     interface getAllCircles {
         @With @Data @AllArgsConstructor class Response {
@@ -77,6 +94,31 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
             public String subject;
             public String content;
             public Long writerId;
+        }
+    }
+    interface postLettersReply {
+        @With @Data @AllArgsConstructor class Request {
+            public String content;
+            public Long writerId;
+        }
+
+        @With @Data @AllArgsConstructor class Response {
+            public Long letterId;
+            public ZonedDateTime date;
+            public Long circlesIds;
+            public String subject;
+            public String content;
+            public Long writerId;
+        }
+    }
+
+    interface deleteCircles {
+        @With @Data @AllArgsConstructor class Request {
+
+        }
+
+        @With @Data @AllArgsConstructor class Response {
+
         }
     }
 
--- java/src/main/java/com/voltaired/voltaired/presentation/resource/UserResource.java
+++ java/src/main/java/com/voltaired/voltaired/presentation/resource/UserResource.java
@@ -1,23 +1,49 @@
 package com.voltaired.voltaired.presentation.resource;
 
-import io.quarkus.security.Authenticated;
-import io.quarkus.security.identity.SecurityIdentity;
+import org.eclipse.microprofile.jwt.JsonWebToken;
 
+import javax.annotation.security.PermitAll;
 import javax.inject.Inject;
 import javax.ws.rs.GET;
+import javax.ws.rs.InternalServerErrorException;
 import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.SecurityContext;
 
 @Path("/users")
-@Authenticated
 public class UserResource {
 
-//    @Inject SecurityIdentity securityIdentity;
+    @Inject
+    JsonWebToken jwt;
 
     @GET
     @Path("/me")
-    public String me() {
-        return "nobody";
-//        return securityIdentity.getPrincipal().getName();
+    @PermitAll
+    @Produces(MediaType.TEXT_PLAIN)
+    public String me(@Context SecurityContext ctx) {
+        return getResponseString(ctx);
+    }
+
+    private String getResponseString(SecurityContext ctx) {
+        String name;
+        if (ctx.getUserPrincipal() == null) {
+            name = "anonymous";
+        } else if (!ctx.getUserPrincipal().getName().equals(jwt.getName())) {
+            throw new InternalServerErrorException("Principal and JsonWebToken names do not match");
+        } else {
+            name = ctx.getUserPrincipal().getName();
+        }
+        return String.format("hello + %s,"
+                             + " isHttps: %s,"
+                             + " authScheme: %s,"
+                             + " hasJWT: %s",
+                             name, ctx.isSecure(), ctx.getAuthenticationScheme(), hasJwt());
+    }
+
+    private boolean hasJwt() {
+        return jwt.getClaimNames() != null;
     }
 
 }
